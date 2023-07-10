c12c396c4ef165990243b7edb4449aba9386eeff
Living Broscillograph <totservis@gmail.com>
2023-07-10 12:19:24 +0300

    Implement ImGuiContext fix as a hack

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <BSE_Client.h>
-// #include "BSE_Client.h"
-// #include <vendor/imgui/imgui.h>
-// #include <Core.h>
-// #include <log.h>
-// #include <Application.h>
-// #include <EntryPoint.h>
+
+#include "hacks.h"
 
 class ExampleLayer : public BSE::Layer {
 public:
@@ -13,39 +9,22 @@ public:
 	{};
 	
 	void OnUpdate() override {
-		//BSE_INFO("ExampleLayer - Update!");
 	}
 	
 	void OnEvent(BSE::Event& event) override {
-		// BSE_TRACE("ExampleLayer: {0}", event);
 	}
 };
 
 
 class SandboxGuiLayer : public BSE::ImGuiLayer {
 	void OnImGuiRender() {
-		// ------------------------------------------------------------------------------------
-		// Hard-copy from FixImGuiContext defined in ImGuiLayer.h/cpp	-----------------------
-		if (m_ImGuiContext != nullptr){									// --------------------
-			if (m_ImGuiContext != ImGui::GetCurrentContext()){ 			// --------------------
-				// from imgui.h & imgui.cpp comments: 					-----------------------
-				// if using Dear ImGui as a shared library, it is necessary to use
-				// SetCurrentContext() and SetAllocatorFunctions() to restore normal behaviour
-				BSE_TRACE("SandboxGui: Current Context is different!"); // --------------------
-				ImGui::SetCurrentContext(m_ImGuiContext);				// --------------------
-				BSE_TRACE("SandboxGui: Current Context is set again!"); // --------------------
-			} // ------------------------------------------------------------------------------
-		} // ----------------------------------------------------------------------------------
-		// ------------------------------------------------------------------------------------
+		FixImGuiContext(m_ImGuiContext);
 			
 		static char buf[256] = u8"Фыва";
 		
 		static char textinput[256] = u8"Wonder what это такое?";
 		static float f = 0.0f;
 		
-		// DONE: find out why this crashes the app
-		// calling to ImGui::GetIO() tells that no context is availiable
-		// GetCurrentContext returned wrong context! Synchronized it with m_ImGuiContext
 		ImGui::Begin(u8"Начало");
 		ImGui::Text(u8"Превед, мир!!!!! %d", 111);
 		ImGui::Button(u8"Сохранить");
@@ -65,7 +44,6 @@ public:
 		
 		SetImGuiLayer(new SandboxGuiLayer());
 		PushOverlay(GetImGuiLayer());
-		//PushOverlay(new BSE::ImGuiLayer());
 		
 		m_ImGuiLayerEnabled = false;
 	}
